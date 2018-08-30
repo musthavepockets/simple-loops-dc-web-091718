518deb31d5144235590648cd186ce5407fa47213
cd <coraldarling@outlook.com>
2018-08-30 20:06:07 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,4 @@
-require 'pry'
 # REMEMBER: print your output to the terminal using 'puts'
-binding.pry
 def loop_iterator(number_of_times)
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
